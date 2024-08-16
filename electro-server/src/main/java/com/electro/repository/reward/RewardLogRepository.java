4491f6e279952fd91b8edc81d21b7064d8cdcebb
fuocy <fuocy.huynh@gmail.com>
2024-08-16 09:20:42 +0700

    Setup Redux

---
+++ electro-server/src/main/java/com/electro/repository/reward/RewardLogRepository.java
@@ -0,0 +1,18 @@
+package com.electro.repository.reward;
+
+import com.electro.entity.reward.RewardLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RewardLogRepository extends JpaRepository<RewardLog, Long>, JpaSpecificationExecutor<RewardLog> {
+
+    @Query("SELECT COALESCE(SUM(r.score),0) FROM RewardLog r JOIN r.user u WHERE u.username = :username")
+    int sumScoreByUsername(@Param("username") String username);
+
+    List<RewardLog> findByUserUsername(String username);
+
+}
